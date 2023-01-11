2472188642bc9f164ecb5f3a4e98cc6bef2fe3d5
Yaroslav Solomianyi <Soman74geeev@gmail.com>
2023-01-11 16:08:12 +0200

    setting linter

--- src/api/signUp/signUp.tsx
+++ src/api/signUp/signUp.tsx
@@ -33,7 +33,8 @@ export const useSignUp = (): IUseSignUp => {
   const [data, setData] = useState<ISignUpData | null>(null);
 
   const signUp = (params: ISignUpParams) => {
-    axios.post(`${process.env.REACT_APP_API_URL}/api/auth/signUp`, params, { })
+    axios
+      .post(`${process.env.REACT_APP_API_URL}/api/auth/signUp`, params, {})
       .then((response: AxiosResponse<ISignUpData | null>) => {
         console.log(response.data);
       });
